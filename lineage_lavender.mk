d0e62cebfe0049f0304da48d02c4579e1f0dda7f
Prashant <93838092+ImPrashantt@users.noreply.github.com>
2022-04-11 15:12:00 +0530

    lavender: Inherit gapps

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -11,10 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
+
+# Inherit GApps (from Project Awaken)
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_lavender
